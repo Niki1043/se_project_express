f6ce89ec8be64d5fec647ff14da05bcc53bd5d5f
Nikita Patel <npatel.1043@gmail.com>
2023-04-20 11:48:55 -0700

    Sprint12PatchFix

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const user = require("./users");
 const clothingItem = require("./clothingItems");
+const { ID_NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", user);
 router.use("/items", clothingItem);
 
 router.use((req, res) => {
-  res.status(500).send({ message: "Router not found" });
+  res.status(ID_NOT_FOUND).send({ message: "Router not found" });
 });
 
 module.exports = router;
