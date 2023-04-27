c8c6319a68116de0071e6676ea3d6a8bb7d38f34
Nikita Patel <npatel.1043@gmail.com>
2023-04-27 12:56:43 -0700

    AddMiddlewarestoClothingRoutes

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -6,20 +6,21 @@ const {
   likeClothingItem,
   dislikeClothingItem,
 } = require("../controllers/clothingItems");
+const auth = require("../middlewares/auth");
 
 // GET /items — returns all clothing items
 router.get("/", getClothingItems);
 
 // POST /items — creates a new item
-router.post("/", createClothingItem);
+router.post("/", auth, createClothingItem);
 
 // DELETE /items/:itemId — deletes an item by _id
-router.delete("/:itemId", deleteClothingItem);
+router.delete("/:itemId", auth, deleteClothingItem);
 
 // PUT /items/:itemId/likes — like an item
-router.put("/:itemId/likes", likeClothingItem);
+router.put("/:itemId/likes", auth, likeClothingItem);
 
 // DELETE /items/:itemId/likes — unlike an item
-router.delete("/:itemId/likes", dislikeClothingItem);
+router.delete("/:itemId/likes", auth, dislikeClothingItem);
 
 module.exports = router;
