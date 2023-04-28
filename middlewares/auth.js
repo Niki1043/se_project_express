e72ec1e65d22a376698f72d981e2fb1c9887bc4b
Nikita Patel <npatel.1043@gmail.com>
2023-04-28 12:36:13 -0700

    Sprint13JWTTokenerrorfix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,11 @@
 const jwt = require("jsonwebtoken");
 const { UNAUTHORIZED_ERROR } = require("../utils/errors");
-const { JWT_SECRET } = require("../utils/config");
+const JWT_SECRET = require("../utils/config");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(authorization);
+  console.log(req.headers);
   if (!authorization || !authorization.startsWith("Bearer ")) {
     return res
       .status(UNAUTHORIZED_ERROR)
