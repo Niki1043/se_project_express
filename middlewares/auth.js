ad9bb383e0311fd27add1e984e737dd69dd90659
Nikita Patel <npatel.1043@gmail.com>
2023-05-23 09:42:37 -0700

    Sprint15MiddlewareErrorHandling

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,22 @@
 const jwt = require("jsonwebtoken");
-const { UNAUTHORIZED_ERROR } = require("../utils/errors");
+
+const UnauthorizedError = require("../errors/UnauthorizedError");
 const JWT_SECRET = require("../utils/config");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res
-      .status(UNAUTHORIZED_ERROR)
-      .send({ message: "Authorization required" });
+    return Promise.reject(new UnauthorizedError("Authorization required"));
   }
+
   const token = authorization.replace("Bearer ", "");
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    // console.error(err);
-    return res
-      .status(UNAUTHORIZED_ERROR)
-      .send({ message: "Authorization required" });
+    return Promise.reject(new UnauthorizedError("Authorization required"));
   }
+
   req.user = payload;
   return next();
 };
